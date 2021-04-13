0228e9a5514d9aa009a163d7b319822ea3033c2d
RandyCB <randycorderob@gmail.com>
2021-04-13 15:23:53 -0600

    Multiprocessing file update

--- Multiprocessing.py
+++ Multiprocessing.py
@@ -5,9 +5,22 @@ import time
 """
 Description:
 	Python 3.8.5 
-Notes:
+	Program example for multiprocessing using two different methods
 	
+Notes:
+	Multiprocessing is meant to be use in tasks that make high use of the CPU cores
+	Using the contex manager will join processes automatically 
+	The map method executes the function argument with all the elements of the list argument
+	
+Test case:
+	Intel(R) Core(TM) i3-4005U CPU @ 1.70GHz
+	On-line CPU(s) list:             0-3
+	Thread(s) per core:              2
+	20.04.2 LTS (Focal Fossa)
 
+	Running the Process pool with more than 4 iterations at time
+	will cause to execute in more than 1 second 
+	since it is related with the HW cores in the PC where it is running	
 """
 
 
@@ -18,7 +31,7 @@ def do_something(seconds):
 	
 def do_something_else(seconds):
 	print(f'Sleeping {seconds} second(s)')
-	time.sleep(1)
+	time.sleep(seconds)
 	return 'Done sleeping'
 
 
@@ -47,12 +60,30 @@ start = time.perf_counter()
 
 with concurrent.futures.ProcessPoolExecutor() as executor:
 
-	results = [executor.submit(do_something_else,1) for _ in range(4) ]	
+	results = [executor.submit(do_something_else,1) for _ in range(5) ]	
 	for f in concurrent.futures.as_completed(results):
 		print(f.result())	
 
 finish = time.perf_counter()
 print(f'Finished in {round(finish-start,3)} seconds(s)')
+
+print(" "*3)
+#-------------------------------------------------------------------------------
+#Using map function instead
+
+start = time.perf_counter()
+s = [5,4,3,2,1]
+with concurrent.futures.ProcessPoolExecutor() as executor:
+	results = executor.map(do_something_else, s)
+
+	for result in results:
+		print(result)
+
+
+finish = time.perf_counter()
+print(f'Finished in {round(finish-start,3)} seconds(s)')
+
+
 
 
 
